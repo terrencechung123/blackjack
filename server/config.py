d33ffc3e7de3c3161891888b3d0e7b9329227d23
Terrence Chung <terrencechung35@gmail.com>
2023-04-06 01:27:30 -0700

    added login

--- server/config.py
+++ server/config.py
@@ -14,6 +14,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
+app.secret_key = 'mysecretlittlekey'
+
 migrate = Migrate(app, db)
 db.init_app(app)
 
